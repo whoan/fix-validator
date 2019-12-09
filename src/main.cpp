15bad6eff11573d3de36fae6dc959e4249d6eb13
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-09 08:25:13 -0300

    Improve error message

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,8 @@ int main() try {
   return 0;
 } catch (const std::exception& ex) {
   std::cerr << ex.what() << std::endl;
+  if (std::string(ex.what()).find("Could not parse data dictionary stream") != std::string::npos) {
+    std::cerr << "To see more details, run: xmllint --noout <document>.xml  # part of docbook-xml package" << std::endl;
+  }
   return 1;
 }
